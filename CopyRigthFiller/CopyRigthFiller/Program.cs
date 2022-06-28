f05e4ef114e92d850f9c26237be1cba482f0e4f7
Yuan Qi Zheng <yuan.zheng@evosoft.com>
2022-06-28 15:17:07 +0200

    template fixed

--- CopyRigthFiller/CopyRigthFiller/Program.cs
+++ CopyRigthFiller/CopyRigthFiller/Program.cs
@@ -7,6 +7,7 @@ using CopyRigthFiller.Data;
 using CopyRigthFiller.Logger;
 using CopyRigthFiller.Logic;
 using Interfaces.CopyRightFiller.Logger;
+using Interfaces.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -20,21 +21,30 @@ namespace CopyRigthFiller
             Console.WriteLine("Hello World!");
 
             Configs configurations = JsonParser.GetConfig();
-
-            List<ILogger> loggers = new List<ILogger>() 
+            List<ILogger> loggers = new List<ILogger>()
             {
                 new ConsoleLogger(),
-                new FileLogger(configurations.LogFilepath) 
+                new FileLogger(configurations.LogFilepath)
             };
             ILoggerManager manager = new LoggerManager(loggers);
 
-            manager.WriteLine("Tool started");
-            GetFiles files = new GetFiles(manager,configurations);
-            var allFiles = files.GetAllFiles();
+            try
+            {
+                manager.WriteLine("Tool started");
+                GetFiles files = new GetFiles(manager, configurations);
+                var allFiles = files.GetAllFiles();
 
-            FileHandlingLogic logic = new FileHandlingLogic(manager,configurations);
-            logic.TryFixFiles(allFiles);
-
+                FileHandlingLogic logic = new FileHandlingLogic(manager, configurations);
+                logic.TryFixFiles(allFiles);
+            }
+            catch(UserException ex)
+            {
+                manager.WriteLine(ex.ToString());
+            }
+            catch(Exception ex)
+            {
+                manager.WriteLine(ex.ToString());
+            }
         }
     }
 }
